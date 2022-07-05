567c35d2705bef981c57b7898187996a1a7d176a
salenctawer <salenctawer@gmail.com>
2022-07-05 23:13:39 +0500

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Notes from './components/Notes/Notes';
+import UserInfo from './components/UserInfo/UserInfo';
 
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <UserInfo />
+      <Notes />
     </div>
   );
 }
