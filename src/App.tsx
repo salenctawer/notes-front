a0a0650759b53a8b4e5c3b436da1cacdb2e2cf0d
salenctawer <salenctawer@gmail.com>
2022-07-06 15:14:51 +0500

    header layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Notes from './components/Notes/Notes';
 import UserInfo from './components/UserInfo/UserInfo';
+import Container from '@mui/material/Container';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <UserInfo />
-      <Notes />
+      <Container maxWidth='lg'>
+        <UserInfo />
+        <Notes />
+      </Container>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,74 @@
+import { Button, Modal, Typography, Box, Container } from "@mui/material";
 import React from "react";
+import { useState } from "react";
+import s from './Header.module.scss'
+import { useNavigate } from 'react-router-dom';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '1px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
 
 const Header = () =>{
-    return(<div>
-        header
-    </div>)
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const isAuth = false
+
+    const onClickLogout = () => {};
+
+    const redirect = useNavigate()
+
+    return(<section className={s.header}>
+        <Container maxWidth='lg'>
+        <div className={s.container}>
+        <Typography>
+            Создание заметок
+        </Typography>
+        {
+            isAuth? (
+                <div className={s.buttons}>
+                    <Button onClick={handleOpen}>
+                        Создать заметку
+                    </Button>
+                    <Button onClick={onClickLogout} variant="contained" color="error">
+                        Выйти
+                    </Button>
+                </div>
+            ) : (
+                <div className={s.buttons}>
+                    <Button variant="outlined" onClick={()=>redirect('/login')}>Войти</Button>
+                    <Button variant="contained" onClick={()=>redirect('/register')}>Создать аккаунт</Button>
+                </div>
+            )
+        }
+
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" onClick={handleClose}>
+            Clear cache
+          </Typography>
+          <Typography id="modal-modal-title" sx={{ mt: 2 }} onClick={handleClose}>
+            Logging out
+          </Typography>
+        </Box>
+      </Modal>
+      </div>
+      </Container>
+    </section>)
 }
 
 export default Header
