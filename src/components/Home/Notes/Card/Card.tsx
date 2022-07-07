9966123ad1263cfd58bf3bf752472c4437e50030
salenctawer <salenctawer@gmail.com>
2022-07-07 20:29:55 +0500

    card styled

--- src/components/Home/Notes/Card/Card.tsx
+++ src/components/Home/Notes/Card/Card.tsx
@@ -1,17 +1,40 @@
 import React from "react"
 import s from './Card.module.scss'
+import { Typography } from "@mui/material";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 
 
 interface CardProps {
     title: String,
     _id: String,
     text: String,
-    deadline: String
+    deadline: String,
+    important: String
 }
 
 const Card: React.FC<CardProps> = (props) =>{
     return(<div className={s.card}>
-        
+        <div className={s.titleDeleteContainer}>
+            <div className={s.title}>
+                <Typography>{props.title}</Typography>
+            </div>
+            <div className={s.delete}>
+            <DeleteOutlineIcon/>
+            </div>
+        </div>
+        <div>
+            <Typography>{props.important}</Typography>
+        </div>
+        <div>
+            <Typography>{props.deadline}</Typography>
+        </div>
+        <div className={s.openFullContainer}>
+            <Typography style={{marginRight: '10px'}}>
+                Открыть полностью
+            </Typography>
+            <OpenInFullIcon />
+        </div>
     </div>)
 }
 
--- src/components/Home/Notes/Notes.tsx
+++ src/components/Home/Notes/Notes.tsx
@@ -9,13 +9,6 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Card from "./Card/Card";
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
 
 const Notes: React.FC = () =>{
 
@@ -30,23 +23,22 @@ const Notes: React.FC = () =>{
     }, [dispatch])
 
     return(<div className={s.notes}>
+        <Box sx={{ width: '100%' }}></Box>
+        <Grid container rowSpacing={6} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {
             isNotesLoading? <div>Идет загрузка</div>: notes.map((obj, index)=>
-            <Box sx={{ width: '100%' }}>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-              <Grid item xs={6}>
+              <Grid item xs={4}>
                 <Card 
                     title={obj.title}
                     deadline={obj.deadline}
                     _id={obj._id}
                     text={obj.text}
-                
+                    important={obj.important}
                 />
               </Grid>
-            </Grid>
-          </Box>
             ) 
         }
+        </Grid>
 
     </div>)
 }
