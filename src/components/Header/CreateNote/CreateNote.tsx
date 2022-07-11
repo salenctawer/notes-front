dd4b2f6718ccc9810046baa5faa71e5b5e098716
salenctawer <salenctawer@gmail.com>
2022-07-11 14:44:14 +0500

    date picker was added

--- src/components/Header/CreateNote/CreateNote.tsx
+++ src/components/Header/CreateNote/CreateNote.tsx
@@ -1,5 +1,28 @@
-import {Box} from "@mui/material";
+import React, { useState } from "react";
+import {Box, Button, TextField} from "@mui/material";
+import s from './CreateNote.module.scss'
+import { useForm } from 'react-hook-form';
+import { AddNoteType } from "../../../types/types";
+import MenuItem from '@mui/material/MenuItem';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
+
+const selectItems = [
+    {
+        value: 'Очень важно'
+    },
+    {
+        value:'Важно'
+    },
+    {
+        value:'Средне'
+    },
+    {
+        value:'Не очень важно'
+    },
+]
 
 const style = {
     position: 'absolute',
@@ -13,8 +36,86 @@ const style = {
 }
 
 const CreatePost = () =>{
+
+    const {register, handleSubmit, setValue, formState: {errors}} = useForm<AddNoteType>()
+
+    const [data, setData] = useState<Date | null>(null);
+
+    const onSubmit = (values:AddNoteType ) =>{
+        console.log(values)
+    }
+
     return(<Box sx={style}>
-        Создать пост
+        <form onSubmit={handleSubmit((values)=>onSubmit(values))}>
+          <TextField
+              {...register("title", {
+                required: {
+                  value: true,
+                  message: "Это поле обязательно",
+                },
+              })}
+              autoFocus
+              margin="dense"
+              id="title"
+              label="Название заметки"
+              type="text"
+              fullWidth
+              variant="standard"
+              error={Boolean(errors.title?.message)}
+              helperText={errors.title?.message}
+            />
+            <TextField
+                {...register("text", {
+                    required: {
+                    value: true,
+                    message: "Это поле обязательно",
+                    },
+                })}
+                margin="dense"
+                id="text"
+                label="Текст заметки"
+                fullWidth
+                multiline
+                variant="standard"
+            />
+             <TextField
+                {...register("important", {
+                    required: {
+                    value: true,
+                    message: "Это поле обязательно",
+                    },
+                })}
+                id="important"
+                select
+                label="Важность"
+                style={{width: 125}}
+                >
+                {selectItems.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                    label="Basic example"
+                    value={data}
+                    onChange={(newValue) => {
+                    setData(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+            </LocalizationProvider>
+             <Button 
+                variant="contained"
+                type='submit'
+                sx={{
+                marginTop: 2
+                }}
+            >
+              Готово
+          </Button>
+        </form>
     </Box>)
 }
 
